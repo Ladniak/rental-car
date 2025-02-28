89f681c0872993df3284a43d1fe5bb0281585386
Netishyn <sergeyladnyak7@gmail.com>
2025-02-28 17:03:38 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-function App() {
+import { Route, Routes } from 'react-router-dom'
 
+import HomePage from './pages/HomePage/HomePage'
+import CatalogPage from './pages/CatalogPage/CatalogPage'
+import CarPage from './pages/CarPage/CarPage'
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
+
+import Navigation from './components/Navigation/Navigation'
+
+function App() {
   return (
     <>
-
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/catalog/:id" element={<CarPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   )
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,15 @@
+import { Link } from "react-router-dom"
+import module from "./HomePage.module.css"
+
 const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <div className={module.container}>
+            <div className={module.info}>
+                <h1 className={module.header}>Find your perfect rental car</h1>
+                <p className={module.paragraph}>Reliable and budget-friendly rentals for any journey</p>
+                <Link className={module.linkButton} to="/catalog">View Catalog</Link>
+            </div>
+        </div>
     )
 }
 
